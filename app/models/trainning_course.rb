5d54549a2890b4e810d03397a3736dbe5d14c2ae
Fernando Ruiz <fruizg0302@gmail.com>
2014-06-07 15:58:48 -0500

    Relaciones en traning course

--- app/models/trainning_course.rb
+++ app/models/trainning_course.rb
@@ -1,5 +1,10 @@
 class TrainningCourse < ActiveRecord::Base
   attr_accessible :course_id, :course_status_id, :education_type_id, :language_id, :municipality_id, :no_asistencias, :fecha_inicio, :fecha_fin
+  belongs_to :municipality
+  belongs_to :course
+  belongs_to :education_type
+  belongs_to :language
+  belongs_to :course_status
 
 
 end

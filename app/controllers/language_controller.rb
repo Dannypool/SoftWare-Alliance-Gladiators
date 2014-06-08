ed1433f4eef6ca21976a197360d2a6a72f38cf1f
Fernando Ruiz <fruizg0302@gmail.com>
2014-06-07 23:40:23 -0500

    cambio en el controlador de lenguaje una vez mas

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,17 +1,22 @@
 class LanguageController < InheritedResources::Base
   respond_to :json
-  #def index
-  #end
+  def index
+  respond_with Language.all
+  end
 
-  #def show
-  #end
+  def show
+  respond_with Language.find(params[:id])
+  end
 
-  #def create
-  #end
+  def create
+    respond_with Language.create(params[:language])
+  end
 
-  #def update
-  #end
+  def update
+  respond_with Language.update(params[:id],params[:language])
+  end
 
-  #def destroy
-  #end
+  def destroy
+  respond_with Language.destroy(params[:id])
+  end
 end

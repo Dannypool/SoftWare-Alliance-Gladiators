ab81b9b4694c6e669a26d09650f06e552c300c3d
Fernando Ruiz <fruizg0302@gmail.com>
2014-06-08 17:40:02 -0500

    cambios en metodos de insercion anidada

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,16 @@ class Person < ActiveRecord::Base
   attr_accessible :a_materno, :a_paterno, :calzado, :curp, :fecha_nac, :language_id, :lugar_nac, :nombre, :rfc, :sexo, :talla_pants, :talla_playera, :telefono, :tipo_sangre, :tipo_zona
   has_many :addresses
   has_many :allocated_figures
+  has_many :role_statuses
+  has_many :educations
   belongs_to :language
   has_one :survey
+
+
+  accepts_nested_attributes_for :addresses
+  accepts_nested_attributes_for :allocated_figures
+  accepts_nested_attributes_for :role_statuses
+  accepts_nested_attributes_for :educations
+
 
 end

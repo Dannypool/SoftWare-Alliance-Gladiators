d43da0a81a5a26c937c727375a7b46a618a46c4d
Fernando Ruiz <fruizg0302@gmail.com>
2014-06-09 15:38:39 -0500

    correciones en controlador de persona por que devolvía muchos registros

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,7 +12,7 @@ class PeopleController < ApplicationController
     #@data = People.where("state_id = ?", params[:id]).select([:id, :municipio, :state_id])
     sql = "select p.id, p.nombre, p.a_paterno, lc.localidad, mp.municipio, st.estado from people as p inner join addresses as ad on p.id = ad.person_id
           inner join localities as lc on lc.id = ad.locality_id inner join municipalities as mp on mp.id = lc.municipality_id
-          inner join states as st on st.id = mp.state_id where st.id =  " + params[:id] + "limit 1"
+          inner join states as st on st.id = mp.state_id where st.id =  " + params[:id]
 
     @objeto_pg = ActiveRecord::Base.connection.execute(sql)
     render :json => @objeto_pg

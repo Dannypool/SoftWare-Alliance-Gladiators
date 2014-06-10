e3adc3133dbe72e55cb1b64c589e681816119fb6
Dannypool <a.kluya@gmail.com>
2014-06-10 02:24:38 -0500

    view de asignacion de cursos

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -27,8 +27,10 @@ class CoursesController < ApplicationController
 
   def show_courses_by_municipality
 
-    sql = "select cr.id, cr.description, cr.status, mn.municipio from courses as cr
+    sql = "select cr.id, cr.description, cr.status, mn.municipio, ed.nombre as educacion, lan.nombre as lengua , lan.id  as lengua_id from courses as cr
           inner join trainning_courses as tc on tc.course_id = cr.id
+          inner join education_types as ed on ed.id = tc.education_type_id
+          inner join languages as lan on  lan.id = tc.language_id
           inner join municipalities as mn on tc.municipality_id = mn.id where mn.id = " + params[:id]
 
     @courses_by_municipality_pg = ActiveRecord::Base.connection.execute(sql)
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,11 @@ GestionFe::Application.routes.draw do
     match 'consulta_de_las_actividades/:id' => 'activities#consulta_actividades'
     match 'consulta_de_padron/' => 'padron#show_people_from_padron'
     match 'consulta_personas_asignadas/' => 'people#show_people_from_state'
+    match 'consulta_asignadas/' => 'people#show_people_from_asignados'
     match 'consulta_localidades/:id' => 'localities#show_many_municipalities'
     match 'cursos_habilitados' => 'courses#show_habilitados'
     match 'consulta_municipios/:id' => 'municipalities#show_many'
+
     resources :padron
     resources :people
     resources :training_courses

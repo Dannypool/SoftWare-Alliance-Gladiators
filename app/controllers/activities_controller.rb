ee68d03cabfe7b6c1759c9549a04899edf234edc
Dannypool <a.kluya@gmail.com>
2014-06-09 23:03:19 -0500

    AF: busqueda de sede en formularios

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -79,10 +79,11 @@ class ActivitiesController < ApplicationController
 
     #Aqui espero el id de localidad
 
-    sql = "select loc.localidad, v.localizacion, a.id, l.nombre from localities as loc
-    inner join venues as v on v.locality_id = loc.id
-  inner join activities as a on a.venue_id = v.id
-  inner join languages as l on a.language_id = l.id where loc.id = " + params[:id]
+    sql = "select loc.localidad, v.localizacion, a.id, l.nombre, et.nombre from localities as loc
+inner join venues as v on v.locality_id = loc.id
+inner join activities as a on a.venue_id = v.id
+inner join education_types as et on a.education_type_id = et.id
+inner join languages as l on a.language_id = l.id where loc.id =" + params[:id]
 
     @actividad_pg = ActiveRecord::Base.connection.execute(sql)
     render :json =>  @actividad_pg
@@ -93,7 +94,7 @@ class ActivitiesController < ApplicationController
 
     #Aqui espero el id de localidad
 
-    sql = "select et.nombre as educacion, l.nombre as lengua, a.id from activities as a
+    sql = "select et.nombre as educacion, l.nombre as lengua, a.id , a.necesidad from activities as a
             inner join languages as l on a.language_id = l.id
             inner join education_types as et on a.education_type_id = et.id
             where a.id = " + params[:id]
